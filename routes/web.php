a17e0b932da8cee96f250fe8b38725dbde0901fe
蔡名彥 <a976543257@gmail.com>
2019-02-23 05:01:33 +0800

    createRoute

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,24 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/about', function () {
+    return 'about';
+});
+
+Route::get('/contact', function () {
+    return 'contact';
+});
+
+Route::get('/posts', function () {
+    return 'posts';
+});
+
+Route::get('/posts/{id}', function ($id) {
+    return 'contact ' . $id;
 });
